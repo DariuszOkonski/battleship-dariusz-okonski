9116fd6dc8ec9846ba0bd9c747e77fd9a46e840f
Pekler95 <krzyskos95@gmail.com>
2021-05-25 13:35:56 +0200

    Add packages and class Game

---
+++ src/main/java/com/inout/Display.java
@@ -0,0 +1,19 @@
+package com.inout;
+
+public class Display {
+    public void printGameMenu(){
+        System.out.println("GAME MENU");
+    }
+
+    public void printShipPlacementProcess(){
+        System.out.println("SHIP PLACEMENT PROCESS");
+    }
+
+    public void printGamePlay(){
+        System.out.println("GAME PLAY");
+    }
+
+    public void printGameOver(){
+        System.out.println("PRINT GAME OVER");
+    }
+}
