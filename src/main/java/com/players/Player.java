dd6e702861d2ad78c6b9bac848728407d822a9d6
Dariusz Okonski <darek200180@gmail.com>
2021-05-29 17:40:56 +0100

    build basic Player class

--- src/main/java/com/players/Player.java
+++ src/main/java/com/players/Player.java
@@ -1,4 +1,25 @@
 package com.players;
 
+import com.ships.Ship;
+
+import java.util.List;
+
 public class Player {
+    private List<Ship> ships;
+    private Board board;
+    private String name;
+
+    public Player(List<Ship> ships, Board board, String name) {
+        this.ships = ships;
+        this.board = board;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
 }
